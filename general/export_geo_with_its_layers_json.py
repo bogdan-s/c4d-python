557a723c7a3b3b988b5861d203c568bf06111ac2
Ben Traje <52109229+bentraje@users.noreply.github.com>
2020-01-20 17:28:25 +0800

    Create export_geo_with_its_layers_json.py

---
+++ general/export_geo_with_its_layers_json.py
@@ -0,0 +1,27 @@
+# FBX import in Blender does not support hierarchical grouping when a geometry is skinned
+# The output file of this script is used for another script in Blender
+
+
+import c4d
+import json
+
+file = "D:\layer_list.json"
+
+def get_all_objects(op, filter, output):
+    while op:
+        if filter(op):
+            output.append(op)
+        get_all_objects(op.GetDown(), filter, output)
+        op = op.GetNext()
+    return output
+
+all_obj = get_all_objects(doc.GetFirstObject(), lambda x: x.CheckType(c4d.Opolygon), [])
+
+data_dict = {}
+
+for obj in all_obj:
+    data_dict[obj.GetName()] = obj.GetLayerObject(doc).GetName()
+
+with open(file, "w") as write_file:
+    json.dump(data_dict, write_file, indent=4)
+    
